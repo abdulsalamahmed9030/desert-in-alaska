0e74a970b10d521dd6d85cdb2b1e836978f56d77
abdulsalamahmed9030 <abdulsalamahmed9030@gmail.com>
2026-01-23 19:18:06 +0530

    hahah

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,7 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import ProjectCTA from '@/components/ProjectCTA'
+import Projects from '@/components/projects'
 import TopBar from '@/components/TopBar'
 import React from 'react'
 
@@ -8,6 +10,8 @@ const page = () => {
     <>
     <TopBar />
      <Header /> 
+     <Projects />
+     <ProjectCTA />
      <Footer />
    </>
   )
